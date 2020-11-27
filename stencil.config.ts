2ad26349388c4c3eba3186226be8bd772e477e6c
Kévin Perrée <kevin@redradix.com>
2020-11-27 21:16:23 +0100

    making element compatible with older browsers

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,6 +3,17 @@ import { Config } from '@stencil/core';
 export const config: Config = {
   namespace: 'mindlife-custom-battery-rangeslider',
   taskQueue: 'async',
+  buildEs5: 'prod',
+  extras: {
+    cssVarsShim: true,
+    dynamicImportShim: true,
+    shadowDomShim: true,
+    safari10: true,
+    scriptDataOpts: true,
+    appendChildSlotFix: false,
+    cloneNodeFix: false,
+    slotChildNodesFix: true,
+  },
   outputTargets: [
     {
       type: 'dist',
